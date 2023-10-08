7e5646043461a8033368c86524d30bc9bf704fc8
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-08 20:45:58 +0530

    add setRequestLocale to index page

--- src/app/[locale]/(main-public)/page.tsx
+++ src/app/[locale]/(main-public)/page.tsx
@@ -1,4 +1,8 @@
-const IndexPage = () => {
+import { PageProps } from "@/types";
+import { unstable_setRequestLocale as setRequestLocale } from "next-intl/server";
+
+const IndexPage = ({ params: { locale } }: PageProps) => {
+  setRequestLocale(locale);
   return <div className="text-center">IndexPage</div>;
 };
 
