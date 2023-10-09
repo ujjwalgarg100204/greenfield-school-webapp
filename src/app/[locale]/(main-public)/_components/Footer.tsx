8183b0cbd25f3cfb3225d1999ff6d9830e2ae0f3
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-09 10:43:48 +0530

    changes styles of footer

--- src/app/[locale]/(main-public)/_components/Footer.tsx
+++ src/app/[locale]/(main-public)/_components/Footer.tsx
@@ -1,5 +1,5 @@
-import { BsTelephone } from "react-icons/bs";
 import Link from "next/link";
+import { BsTelephone } from "react-icons/bs";
 import { MdOutlineLocationOn } from "react-icons/md";
 
 const Footer = () => {
