5871159395fcc3bf9a99baeba2626eb0feeedb78
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-10-09 11:15:19 +0530

    Mainbody added

--- src/app/[locale]/(main-public)/layout.tsx
+++ src/app/[locale]/(main-public)/layout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import LinksNavbar from "./_components/Navbar/LinksNavbar";
 import MainNavbar from "./_components/Navbar/MainNavbar";
+import MainBody from "./_components/MainBody";
 
 type Props = { children: React.ReactNode };
 
 const MainSiteLayout = ({ children }: Props) => {
   return (
-    <header>
-      <MainNavbar />
-      <LinksNavbar />
-      {children}
-    </header>
+    <>
+      <header>
+        <MainNavbar />
+        <LinksNavbar />
+        {children}
+      </header>
+
+      <main>
+        <MainBody />
+      </main>
+    </>
   );
 };
 
