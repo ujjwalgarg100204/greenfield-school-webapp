b4b29e7fb23e58cd3280e371bbaebb49c0b514bc
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-11 16:52:01 +0530

    add about page

---
+++ src/app/[locale]/(main-public)/school/about-us/page.tsx
@@ -0,0 +1,26 @@
+import SectionHeading from "@/components/ui/SectionHeading";
+import { useTranslations } from "next-intl";
+import SideNavbar from "../../_components/Navbar/SideNavbar";
+
+const AboutPage = () => {
+  const t = useTranslations("Pages.school.sub-links.about");
+  return (
+    <div className="container mx-auto my-12 flex justify-between gap-16 px-6 lg:px-8 xl:px-12 2xl:px-16">
+      <div className="w-2/3 flex-grow space-y-12">
+        <section className="max-w-xl space-y-6 lg:max-w-3xl">
+          <SectionHeading id="about-us">{t("title")}</SectionHeading>
+          <div className="space-y-4">
+            <p className="text-justify">{t("content.para-1")}</p>
+            <p className="text-justify">{t("content.para-2")}</p>
+          </div>
+        </section>
+      </div>
+
+      <div className="hidden w-1/5 lg:block">
+        <SideNavbar linkType="school" selected={{ translationKey: "about" }} />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
