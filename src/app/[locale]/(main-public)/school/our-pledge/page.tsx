9eaf7caa6b66e4965307ea1b2f6387c95e38aa9c
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-10-15 14:18:33 +0530

    pull updated

--- src/app/[locale]/(main-public)/school/our-pledge/page.tsx
+++ src/app/[locale]/(main-public)/school/our-pledge/page.tsx
@@ -1,10 +1,11 @@
-import SectionHeading from "@/components/ui/SectionHeading";
-import { useScopedI18n } from "@/locales/client";
 import type { FC } from "react";
+import SectionHeading from "@/components/ui/SectionHeading";
 import SideNavbar from "../../_components/Navbar/SideNavbar";
+import { getScopedI18n } from "@/locales/server";
+import { useScopedI18n } from "@/locales/client";
 
-const MissionAndVisionPage: FC = () => {
-  const t = useScopedI18n("Pages.school.sub-links.pledge");
+const MissionAndVisionPage: FC = async () => {
+  const t = await getScopedI18n("Pages.school.sub-links.pledge");
 
   return (
     <div className="container mx-auto my-12 flex justify-between gap-16 px-6 lg:px-8 xl:px-12 2xl:px-16">
