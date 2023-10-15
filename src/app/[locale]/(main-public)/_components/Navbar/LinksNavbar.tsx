356b6a2bca4d8a65828a45c34cb2c7637aea5151
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-15 23:26:15 +0530

    replace home button in navbar

--- src/app/[locale]/(main-public)/_components/Navbar/LinksNavbar.tsx
+++ src/app/[locale]/(main-public)/_components/Navbar/LinksNavbar.tsx
@@ -1,10 +1,13 @@
-import { Navbar, NavbarContent } from "@lib/next-ui";
+import { Button, Navbar, NavbarContent } from "@lib/next-ui";
 
 import { NAV_LINKS } from "@/lib/frontend-data";
+import { getScopedI18n } from "@/locales/server";
+import { default as NextLink } from "next/link";
 import type { FC } from "react";
 import LinksNavbarItem from "./LinksNavbarItem";
 
-const LinksNavbar: FC = () => {
+const LinksNavbar: FC = async () => {
+  const t = await getScopedI18n("Pages.home");
   return (
     <Navbar
       height="3"
@@ -16,10 +19,19 @@ const LinksNavbar: FC = () => {
         className="flex flex-wrap gap-y-0 sm:gap-x-8"
         justify="center"
       >
+        <Button
+          as={NextLink}
+          href="/"
+          disableRipple
+          className="bg-slate-400 bg-transparent p-0 text-xs data-[hover=true]:bg-transparent sm:text-sm md:text-base"
+        >
+          {t("title")}
+        </Button>
         {(Object.keys(NAV_LINKS) as Array<keyof typeof NAV_LINKS>).map(
-          navLink => (
-            <LinksNavbarItem key={navLink} linkType={navLink} />
-          ),
+          navLink =>
+            navLink === "home" ? null : (
+              <LinksNavbarItem key={navLink} linkType={navLink} />
+            ),
         )}
       </NavbarContent>
     </Navbar>
