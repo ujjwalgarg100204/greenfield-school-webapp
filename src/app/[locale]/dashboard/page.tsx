a2da636721446160cc17feb9f2662aaaf55db94f
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-20 20:16:33 +0530

    changes styles of dashboard

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,14 +1,20 @@
-import { auth } from "@/app/api/auth/[...nextauth]/route";
 import type { FC } from "react";
+import { auth } from "@/app/api/auth/[...nextauth]/route";
 
 const Dashboard: FC = async () => {
   const session = await auth();
-  console.log("session", JSON.stringify(session, null, 2));
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>{JSON.stringify(session, null, 2)}</p>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="space-y-4 text-center">
+        <h1 className="text-2xl md:text-4xl">
+          Welcome {session?.user.username} to your dashboard
+        </h1>
+        <div>
+          <p>Your Details as received by client:</p>
+          <p>{JSON.stringify(session, null, 2)}</p>
+        </div>
+      </div>
     </div>
   );
 };
