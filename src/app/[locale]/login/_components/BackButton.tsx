bc4e5197e2dd998e240f84f1bdbde3d96abdf880
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-20 20:25:27 +0530

    add back navigation to login page

---
+++ src/app/[locale]/login/_components/BackButton.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { BiArrowBack } from "react-icons/bi";
+import { Button } from "@/lib/next-ui";
+import type { FC } from "react";
+import { useRouter } from "next/navigation";
+
+const BackButton: FC = () => {
+  const router = useRouter();
+  const clickHandler = () => {
+    router.back();
+  };
+
+  return (
+    <Button variant="light" isIconOnly onClick={clickHandler} size="sm">
+      <BiArrowBack className="h-4/5 w-4/5 text-foreground" />
+    </Button>
+  );
+};
+
+export default BackButton;
--- src/app/[locale]/login/_components/Header.tsx
+++ src/app/[locale]/login/_components/Header.tsx
@@ -1,25 +1,29 @@
 import { Navbar, NavbarBrand, NavbarContent } from "@lib/next-ui";
 
-import type { FC } from "react";
 import GreenfieldLogo from "@/../public/images/logo.png";
 import LanguageSwitcher from "@/components/ui/LanguageSwither";
 import NextImage from "next/image";
 import NextLink from "next/link";
+import type { FC } from "react";
+import BackButton from "./BackButton";
 
 const Header: FC = () => {
   return (
     <Navbar position="static">
-      <NavbarBrand as={NextLink} href="/" className="gap-4">
-        <NextImage
-          src={GreenfieldLogo}
-          alt="Greenfield Logo"
-          className="h-12 w-12 rounded-full object-cover object-center"
-          priority
-        />
-        <h1 className="flex-grow font-bold text-black/80 md:text-xl">
-          Greenfield School <br /> Tamil Nadu
-        </h1>
-      </NavbarBrand>
+      <NavbarContent>
+        <BackButton />
+        <NavbarBrand as={NextLink} href="/" className="gap-4">
+          <NextImage
+            src={GreenfieldLogo}
+            alt="Greenfield Logo"
+            className="h-12 w-12 rounded-full object-cover object-center"
+            priority
+          />
+          <h1 className="flex-grow font-bold text-black/80 md:text-xl">
+            Greenfield School <br /> Tamil Nadu
+          </h1>
+        </NavbarBrand>
+      </NavbarContent>
       <NavbarContent justify="end">
         <LanguageSwitcher />
       </NavbarContent>
