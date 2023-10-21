93d189530cc97656496901ade1fcc8cbf7caa217
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-10-22 00:34:42 +0530

    student trips page added

---
+++ src/app/[locale]/(main-public)/student/trips-and-excursions/page.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+
+import ArticlePage from "../../_components/ArticlePage";
+import { NextPageProps } from "@/types";
+import SectionHeading from "@/components/ui/SectionHeading";
+import local from "next/font/local";
+import { setStaticParamsLocale } from "next-international/server";
+
+const TrpisAndExcursion: FC<NextPageProps> = ({ params: { locale } }) => {
+  setStaticParamsLocale(locale);
+  return (
+    <ArticlePage
+      linkType="student"
+      selected={{ translationKey: "certificate" }}
+    >
+      <SectionHeading>Child's Trancfer certificate</SectionHeading>
+      <ul className="list-disc space-y-6">
+        <li className=" text-justify">Add some text here</li>
+        <li className=" text-justify">Add some text here</li>
+        <li className=" text-justify">Add some text here</li>
+      </ul>
+    </ArticlePage>
+  );
+};
+
+export default TrpisAndExcursion;
