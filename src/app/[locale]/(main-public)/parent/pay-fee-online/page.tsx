f1ee029cbd72c206de35690bf062f1b3658be6c5
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-10-23 01:28:06 +0530

    all parent pages added

---
+++ src/app/[locale]/(main-public)/parent/pay-fee-online/page.tsx
@@ -0,0 +1,24 @@
+import ArticlePage from "../../_components/ArticlePage";
+import type { FC } from "react";
+import type { NextPageProps } from "@/types";
+import SectionHeading from "@/components/ui/SectionHeading";
+import { setStaticParamsLocale } from "next-international/server";
+
+const CanteenRules: FC<NextPageProps> = ({ params: { locale } }) => {
+  setStaticParamsLocale(locale);
+  // const t = await getScopedI18n("Pages.student.sub-links.c-rules");
+  return (
+    <ArticlePage linkType="parent" selected={{ translationKey: "fees" }}>
+      <SectionHeading>Online fees Portal</SectionHeading>
+      <ul className="list-disc space-y-6">
+        <li className="text-justify">Add text</li>
+        <li className="text-justify">Add text</li>
+        <li className="text-justify">Add text</li>
+        <li className="text-justify">Add text</li>
+        <li className="text-justify">Add text</li>
+      </ul>
+    </ArticlePage>
+  );
+};
+
+export default CanteenRules;
