4fa75d6db36c93bbfa20bcb1b6913e996a51e957
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-24 00:23:45 +0530

    change loading spinner

--- src/app/[locale]/(main-public)/_components/Navbar/MainNavbar/PrimaryAction.tsx
+++ src/app/[locale]/(main-public)/_components/Navbar/MainNavbar/PrimaryAction.tsx
@@ -1,49 +1,47 @@
 "use client";
 
-import LoadingSpinner from "@/src/components/ui/LoadingSpinner";
-import { useScopedI18n } from "@/src/locales/client";
-import { Button } from "@lib/next-ui";
-import { useSession } from "next-auth/react";
-import NextLink from "next/link";
-import type { FC } from "react";
+import { Button, CircularProgress } from "@lib/next-ui";
+
 import AccountMenu from "./AccountMenu";
+import type { FC } from "react";
+import NextLink from "next/link";
+import { useScopedI18n } from "@/src/locales/client";
+import { useSession } from "next-auth/react";
 
 const PrimaryAction: FC = () => {
   const { status, data } = useSession();
   const t = useScopedI18n("Root.main-navbar");
 
-  if (status === "loading") return <LoadingSpinner />;
+  if (status === "loading")
+    return <CircularProgress aria-label="Loading..." color="primary" />;
+  else if (status === "authenticated") return <AccountMenu />;
+  else
+    return (
+      <>
+        <Button
+          as={NextLink}
+          href="/login"
+          color="primary"
+          variant="solid"
+          className="font-semibold text-white sm:hidden"
+          radius="sm"
+          size="sm"
+        >
+          {t("login")}
+        </Button>
 
-  // if  logged in, show account menu
-  if (data) return <AccountMenu />;
-
-  //if user is not logged in then show login button
-  return (
-    <>
-      <Button
-        as={NextLink}
-        href="/login"
-        color="primary"
-        variant="solid"
-        className="font-semibold text-white sm:hidden"
-        radius="sm"
-        size="sm"
-      >
-        {t("login")}
-      </Button>
-
-      <Button
-        as={NextLink}
-        href="/login"
-        color="primary"
-        variant="solid"
-        className="hidden font-semibold text-white sm:flex"
-        radius="sm"
-      >
-        {t("login")}
-      </Button>
-    </>
-  );
+        <Button
+          as={NextLink}
+          href="/login"
+          color="primary"
+          variant="solid"
+          className="hidden font-semibold text-white sm:flex"
+          radius="sm"
+        >
+          {t("login")}
+        </Button>
+      </>
+    );
 };
 
 export default PrimaryAction;
