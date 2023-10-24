399d36502355c796da77180f564eeb85d7e414ea
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-24 20:52:34 +0530

    update our pledge page

--- src/app/[locale]/(main-public)/school/our-pledge/page.tsx
+++ src/app/[locale]/(main-public)/school/our-pledge/page.tsx
@@ -1,9 +1,9 @@
-import type { NextPageProps } from "@/src/types";
 import ArticleHeading from "@components/ArticleHeading";
+import ArticlePage from "../../_components/ArticlePage";
+import type { FC } from "react";
+import type { NextPageProps } from "@/src/types";
 import { getScopedI18n } from "@locales/server";
 import { setStaticParamsLocale } from "next-international/server";
-import type { FC } from "react";
-import ArticlePage from "../../_components/ArticlePage";
 
 const OurPledgePage: FC<NextPageProps> = async ({ params: { locale } }) => {
   setStaticParamsLocale(locale);
@@ -12,9 +12,17 @@ const OurPledgePage: FC<NextPageProps> = async ({ params: { locale } }) => {
   return (
     <ArticlePage linkType="school" selected={{ translationKey: "pledge" }}>
       <ArticleHeading>{t("title")}</ArticleHeading>
-      <div className="space-y-4">
-        <p className="text-justify">{t("content.para-1")}</p>
-        <p className="text-justify">{t("content.para-2")}</p>
+      <div className="to-primary space-y-2 rounded-md bg-gradient-to-r from-orange-500 via-white p-6 text-center font-semibold text-slate-700/80">
+        <p>India is my country and all Indians are my brothers and sisters.</p>
+        <p>I love my country and I am proud of its rich and varied heritage.</p>
+        <p>I shall always strive to be worthy of it.</p>
+        <p>
+          I shall give respect to my parents, teachers and elders and treat
+          everyone with courtesy.
+        </p>
+        <p>To my country and my people, I pledge my devotion.</p>
+        <p>In their well being and prosperity alone, lies my happiness.</p>
+        <p>Jai Hind!</p>
       </div>
     </ArticlePage>
   );
