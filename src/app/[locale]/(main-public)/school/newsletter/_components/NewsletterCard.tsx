fbff520b558ebf926df822701ee193caecab89d4
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-25 15:30:51 +0530

    add newsletter page

---
+++ src/app/[locale]/(main-public)/school/newsletter/_components/NewsletterCard.tsx
@@ -0,0 +1,31 @@
+import { Card, CardBody, Link } from "@/src/lib/next-ui";
+import NextLink from "next/link";
+import type { FC } from "react";
+import { BsFillFileEarmarkPdfFill } from "react-icons/bs";
+
+type Props = {
+  title: string;
+  link: string;
+};
+
+const NewsletterCard: FC<Props> = ({ title, link }) => {
+  return (
+    <Card isPressable radius="lg" shadow="none" className="border-none">
+      <CardBody className="flex flex-col items-center gap-4">
+        <BsFillFileEarmarkPdfFill className="h-16 w-16 text-red-600" />
+        <Link
+          showAnchorIcon
+          isExternal
+          underline="hover"
+          as={NextLink}
+          href={link}
+          download
+        >
+          {title}
+        </Link>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default NewsletterCard;
---
+++ src/app/[locale]/(main-public)/school/newsletter/page.tsx
@@ -0,0 +1,25 @@
+import ArticleHeading from "@/src/components/ArticleHeading";
+import type { NextPageProps } from "@/src/types";
+import { setStaticParamsLocale } from "next-international/server";
+import type { FC } from "react";
+import ArticlePage from "../../_components/ArticlePage";
+import NewsletterAccordion from "./_components/NewsletterAccordion";
+
+const NewsletterPage: FC<NextPageProps> = ({ params: { locale } }) => {
+  setStaticParamsLocale(locale);
+
+  return (
+    <ArticlePage linkType="school" selected={{ translationKey: "news-letter" }}>
+      <ArticleHeading>Newsletter</ArticleHeading>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim magnam
+        hic eveniet totam accusantium modi iusto nemo dolor, veniam eos
+        aspernatur possimus voluptate aperiam laudantium deleniti suscipit alias
+        tempore fuga?
+      </p>
+      <NewsletterAccordion />
+    </ArticlePage>
+  );
+};
+
+export default NewsletterPage;
