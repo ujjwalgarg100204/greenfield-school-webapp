191875ca98a5747119ba3a597eed4b3047eb3452
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-10-30 22:18:24 +0530

    make School transport fee page server component  Reason: The data being rendered is static and will help in SEO

--- src/app/[locale]/(main-public)/school/school-transport/fees/_components/FeeTable.tsx
+++ src/app/[locale]/(main-public)/school/school-transport/fees/_components/FeeTable.tsx
@@ -1,51 +1,48 @@
-"use client";
-
 import {
   Table,
   TableBody,
   TableCell,
-  TableColumn,
   TableHeader,
+  TableHeaderCell,
   TableRow,
-} from "@lib/next-ui";
+} from "@ui/Table";
+
 import type { FC } from "react";
 
 const FeeTable: FC = () => {
   return (
-    <Table isStriped color="primary">
+    <Table>
       <TableHeader>
-        <TableColumn>Fees Particulars</TableColumn>
-        <TableColumn>Distance (Km)</TableColumn>
-        <TableColumn>Bus Charges Both side (Rs.)</TableColumn>
+        <tr>
+          <TableHeaderCell>Fees Particulars</TableHeaderCell>
+          <TableHeaderCell>Distance (Km)</TableHeaderCell>
+          <TableHeaderCell>Bus Charges Both side (Rs.)</TableHeaderCell>
+        </tr>
       </TableHeader>
       <TableBody>
         <TableRow>
           <TableCell rowSpan={5}>Monthly Bus Fees</TableCell>
-          <TableCell>0 – 05.50 Kms </TableCell>
+          <TableCell>0 - 05.50 Kms </TableCell>
           <TableCell>3,700</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell hidden> </TableCell>
-          <TableCell>05.50 – 10.50 Kms </TableCell>
+          <TableCell>05.50 - 10.50 Kms </TableCell>
           <TableCell>3,850</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell hidden> </TableCell>
-          <TableCell>10.50 – 15.50 Kms </TableCell>
+          <TableCell>10.50 - 15.50 Kms </TableCell>
           <TableCell>4,000</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell hidden> </TableCell>
-          <TableCell>15.50 – 30.00 Kms</TableCell>
+          <TableCell>15.50 - 30.00 Kms</TableCell>
           <TableCell>4,250</TableCell>
         </TableRow>
         <TableRow>
-          <TableCell hidden> </TableCell>
           <TableCell> &gt; 30.00 Kms</TableCell>
           <TableCell>4,350</TableCell>
         </TableRow>
-        <TableRow key="2">
-          <TableCell>Annual – Bus Registration Fees</TableCell>
+        <TableRow>
+          <TableCell>Annual - Bus Registration Fees</TableCell>
           <TableCell>Class Nursery to XII</TableCell>
           <TableCell>500</TableCell>
         </TableRow>
