407f192c3641be0118d6f9aa759d298f08d8b2b9
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-02 22:59:37 +0530

    add trips and excursions page

---
+++ src/app/[locale]/(main-public)/student/trips-and-excursions/data.ts
@@ -0,0 +1,13 @@
+export default [
+  "Nehru Children’s Museum",
+  "Nicco Park / Millennium Park / Zoo",
+  "Science City / Victoria Memorial",
+  "Town Hall",
+  "Birla Planetarium",
+  "Indian Museum",
+  "Energy Park / Eco Park",
+  "Fort William",
+  "Birla Industrial & Technological Museum",
+  "Film Shows",
+  "Jorasanko Museum",
+] as const;
--- src/app/[locale]/(main-public)/student/trips-and-excursions/page.tsx
+++ src/app/[locale]/(main-public)/student/trips-and-excursions/page.tsx
@@ -1,8 +1,10 @@
+import ArticleList from "@/src/components/ArticleList";
 import type { NextPageProps } from "@/src/types";
 import ArticleHeading from "@components/ArticleHeading";
 import { setStaticParamsLocale } from "next-international/server";
 import type { FC } from "react";
 import ArticlePage from "../../_components/ArticlePage";
+import TRIP_DATA from "./data";
 
 const TripsAndExcursionPage: FC<NextPageProps> = ({ params: { locale } }) => {
   setStaticParamsLocale(locale);
@@ -10,11 +12,15 @@ const TripsAndExcursionPage: FC<NextPageProps> = ({ params: { locale } }) => {
   return (
     <ArticlePage linkType="student" selected={{ translationKey: "trips" }}>
       <ArticleHeading>Trips and excursion</ArticleHeading>
-      <ul className="list-disc space-y-6">
-        <li className=" text-justify">Add some text here</li>
-        <li className=" text-justify">Add some text here</li>
-        <li className=" text-justify">Add some text here</li>
-      </ul>
+      <p>
+        Trips are helps for Classes I - XII, and trips will be organized to
+        following places:
+      </p>
+      <ArticleList list={TRIP_DATA} containerClassName="space-y-1.5" />
+      <p className="font-semibold">
+        The School will organize educational trips at the convenience of the
+        Management
+      </p>
     </ArticlePage>
   );
 };
