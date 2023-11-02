d293103a4bcba817853b64b6cdc807c30282965e
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-02 22:50:26 +0530

    remove transfer certificate page

--- src/lib/frontend-data.ts
+++ src/lib/frontend-data.ts
@@ -42,7 +42,6 @@ export const NAV_LINKS = {
     { translationKey: "council", link: "/student/student-council" },
     { translationKey: "activities", link: "/student/co-curricular-activities" },
     { translationKey: "timings", link: "/student/school-timings" },
-    { translationKey: "certificate", link: "/student/transfer-certificate" },
     { translationKey: "prayer", link: "/student/school-prayer" },
     { translationKey: "trips", link: "/student/trips-and-excursions" },
     { translationKey: "cc", link: "/student/code-of-conduct" },
