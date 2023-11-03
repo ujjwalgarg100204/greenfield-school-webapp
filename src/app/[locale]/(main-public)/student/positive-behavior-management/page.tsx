3cf88f2357bcc645bdcc089148c94dabd8686ce8
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-03 14:33:31 +0530

    add positive behavior page

--- src/app/[locale]/(main-public)/student/positive-behavior-management/page.tsx
+++ src/app/[locale]/(main-public)/student/positive-behavior-management/page.tsx
@@ -1,8 +1,10 @@
-import type { NextPageProps } from "@/src/types";
 import ArticleHeading from "@components/ArticleHeading";
-import { setStaticParamsLocale } from "next-international/server";
-import type { FC } from "react";
+import ArticleList from "@/src/components/ArticleList";
 import ArticlePage from "../../_components/ArticlePage";
+import type { FC } from "react";
+import type { NextPageProps } from "@/src/types";
+import POSITIVE_BEHAVIOR_MANAGEMENT_DATA from "./data";
+import { setStaticParamsLocale } from "next-international/server";
 
 const PositiveBehaviorPage: FC<NextPageProps> = ({ params: { locale } }) => {
   setStaticParamsLocale(locale);
@@ -13,11 +15,20 @@ const PositiveBehaviorPage: FC<NextPageProps> = ({ params: { locale } }) => {
       selected={{ translationKey: "+ve-behavior-manage" }}
     >
       <ArticleHeading>Positive Behavior Management</ArticleHeading>
-      <ul className="list-disc space-y-6">
-        <li className=" text-justify">Add some text here</li>
-        <li className=" text-justify">Add some text here</li>
-        <li className=" text-justify">Add some text here</li>
-      </ul>
+      <p>
+        The following disciplinary measures may be adopted by the school in
+        dealing with students behaving in an unruly manner and / or breaching
+        schools disciplinary code/rules.
+      </p>
+      <ArticleList
+        list={POSITIVE_BEHAVIOR_MANAGEMENT_DATA}
+        containerClassName="py-2 space-y-1.5"
+      />
+      <p>
+        Students indulging in aggressive or very violent behaviour will be
+        dropped to and picked up from the school by their parents. They will not
+        be permitted to travel by school bus.
+      </p>
     </ArticlePage>
   );
 };
