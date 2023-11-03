886bf4d201a4174fa677f1034d458d1faeb52fa6
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-03 21:18:02 +0530

    add school infirmary page

--- src/app/[locale]/(main-public)/parent/school-infirmary/page.tsx
+++ src/app/[locale]/(main-public)/parent/school-infirmary/page.tsx
@@ -1,8 +1,10 @@
+import ArticleList from "@/src/components/ArticleList";
 import type { NextPageProps } from "@/src/types";
 import ArticleHeading from "@components/ArticleHeading";
 import { setStaticParamsLocale } from "next-international/server";
 import type { FC } from "react";
 import ArticlePage from "../../_components/ArticlePage";
+import SCHOOL_INFIRMARY_DATA from "./data";
 
 const SchoolInfirmaryPage: FC<NextPageProps> = ({ params: { locale } }) => {
   setStaticParamsLocale(locale);
@@ -10,13 +12,26 @@ const SchoolInfirmaryPage: FC<NextPageProps> = ({ params: { locale } }) => {
   return (
     <ArticlePage linkType="parent" selected={{ translationKey: "infirmary" }}>
       <ArticleHeading>School Infirmary</ArticleHeading>
-      <ul className="list-disc space-y-6">
-        <li className="text-justify"></li>
-        <li className="text-justify"></li>
-        <li className="text-justify"></li>
-        <li className="text-justify"></li>
-        <li className="text-justify"></li>
-      </ul>
+      <p>
+        The School Infirmary is looked after by a qualified nurse. Medical
+        check-up of students are regularly carried out and a record is
+        maintained. If the doctor is of the opinion that a child needs special
+        medical attention, the parents are informed of the same.
+      </p>
+      <p className="font-semibold">
+        Parents are requested not to send sick children to school for attending
+        classes or for appearing for examinations. This is in the interest of
+        the child and at times his/her classmates, as well. Medical Certificates
+        must be submitted so that the child{"'"}s results are not affected.
+      </p>
+
+      <div className="space-y-2">
+        <h3 className="text-xl font-bold">It is important that students: -</h3>
+        <ArticleList
+          list={SCHOOL_INFIRMARY_DATA}
+          containerClassName="space-y-1.5"
+        />
+      </div>
     </ArticlePage>
   );
 };
