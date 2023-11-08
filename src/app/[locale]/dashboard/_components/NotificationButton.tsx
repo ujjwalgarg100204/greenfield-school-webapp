203c9f837644218e693b2e8725b0b8814e9a8c14
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-08 17:23:20 +0530

    add notification button to navbar

---
+++ src/app/[locale]/dashboard/_components/NotificationButton.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@lib/next-ui";
+import { MdNotifications, MdNotificationsActive } from "react-icons/md";
+
+import type { Notification } from "@/src/types";
+import type { FC } from "react";
+import NotificationCard from "./NotificationCard";
+
+const sampleNotifications: Notification[] = [
+  {
+    title: "Sample Notification",
+    description: "This is a sample notification",
+    isUrgent: false,
+  },
+  {
+    title: "Sample Notification",
+    description: "This is a sample notification",
+    isUrgent: false,
+  },
+  {
+    title: "Sample Notification",
+    description: "This is a sample notification",
+    isUrgent: true,
+  },
+];
+
+const notificationUnRead = Math.random() < 0.5;
+
+const NotificationButton: FC = () => {
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button isIconOnly color="primary" variant="light">
+          {notificationUnRead ? (
+            <MdNotificationsActive className="h-6 w-6" />
+          ) : (
+            <MdNotifications className="h-6 w-6" />
+          )}
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu emptyContent={<p>🥳 All caught up 🥳!</p>}>
+        {sampleNotifications.map(notification => (
+          <DropdownItem
+            key={notification.title}
+            className="bg-white p-0 data-[hover=true]:bg-transparent data-[selectable=true]:focus:bg-transparent data-[selectable=true]:focus:text-default-foreground"
+          >
+            <NotificationCard {...notification} />
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default NotificationButton;
