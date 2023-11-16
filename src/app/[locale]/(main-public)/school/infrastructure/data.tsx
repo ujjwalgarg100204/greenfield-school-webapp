9127c4a1f2d380091c35f5ad0609111a6901c0c7
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-11-16 23:25:54 +0530

    data page for infrastructure added and syllabus test bug yet to solve

---
+++ src/app/[locale]/(main-public)/school/infrastructure/data.tsx
@@ -0,0 +1,122 @@
+export const infrastructureCards = [
+  {
+    big_screen: false,
+
+    title: "Smart Classes rooms",
+    description: "Modern & spacious classrooms with Interactive TV",
+    image: {
+      url: "https://images.pexels.com/photos/5905445/pexels-photo-5905445.jpeg?auto=compress&cs=tinysrgb&w=600",
+      alt: "smart class",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Sports facilities",
+    description:
+      "Outdoor Facility for Football, Basketball and Cricket. Indoor Facility for Chess, Gymnastics, Badminton, Yoga & Table Tennis.",
+    image: {
+      url: "https://images.pexels.com/photos/18644293/pexels-photo-18644293/free-photo-of-aerial-drone-view-of-a-rugby-stadium-from-top.jpeg?auto=compress&cs=tinysrgb&w=600",
+      alt: "Sports facilities",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Dance Room",
+    description:
+      "Indian Classical, Folk Dance, Contemporary and Modern Creative Dance etc.",
+    image: {
+      url: "https://images.pexels.com/photos/358010/pexels-photo-358010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Dance Room",
+    },
+  },
+  {
+    big_screen: false,
+    title: "Art Room",
+    description:
+      "Spacious Air-conditioned well equipped Art Studio to enlarge the creative minds.",
+    image: {
+      url: "https://images.pexels.com/photos/383568/pexels-photo-383568.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Art Room",
+    },
+  },
+  {
+    big_screen: false,
+    title: "Music Room",
+    description:
+      "Facilities for learning Piano, Guitar, Tabla, Violin, Vocal - Indian & Western",
+    image: {
+      url: "https://images.pexels.com/photos/164938/pexels-photo-164938.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Music Room",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Computer Lab",
+    description:
+      "7 fully Air-conditioned Computer Labs with 1:1 computer for every student. tie-up with NIIT",
+    image: {
+      url: "https://images.pexels.com/photos/3747486/pexels-photo-3747486.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Computer Lab",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Library",
+    description:
+      "5 airconditioned library to cater 200 students at a time. It is well equipped with 50,000 books, numerous magazines, journals and newspapers with more than 1000 DVDs / CDs to browse through. It has excellent collection of latest Reference books for Competitive Entrance Examinations.",
+    image: {
+      url: "https://images.pexels.com/photos/1370298/pexels-photo-1370298.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Library",
+    },
+  },
+  {
+    big_screen: false,
+    title: "CCTV surveillance system",
+    description:
+      "School is under CCTV surveillance & 24×7 Security Guards to take care of the safety of students. School also has the latest system for fire safety and detection.",
+    image: {
+      url: "https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "CCTV surveillance system",
+    },
+  },
+  {
+    big_screen: false,
+    title: "Science Lab",
+    description:
+      "Twenty-First Century Science Labs to emphasise the modern infrastructure of the school",
+    image: {
+      url: "https://images.pexels.com/photos/4033148/pexels-photo-4033148.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Science Lab",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Infirmary",
+    description:
+      "An airconditioned efficient Infirmary with trained nurse to cater daily medical emergencies. Provision for online medical consultation with trained Doctors of the city.",
+    image: {
+      url: "https://images.pexels.com/photos/13427358/pexels-photo-13427358.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Infirmary",
+    },
+  },
+  {
+    big_screen: true,
+    title: "Internet and WIFI facility",
+    description:
+      "Wi-Fi Campus for conducting Online Exams to prepare students for Competitive Exams.",
+    image: {
+      url: "https://images.pexels.com/photos/13092788/pexels-photo-13092788.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Internet and WIFI facility",
+    },
+  },
+  {
+    big_screen: false,
+    title: "Fire-Fighting Devices",
+    description:
+      "Fire Control Panel, Smoke Detectors, Hooters, Manual Call Point, Water Pump, Reservoir.",
+    image: {
+      url: "https://images.pexels.com/photos/189474/pexels-photo-189474.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      alt: "Fire-Fighting Devices",
+    },
+  },
+];
--- src/app/[locale]/(main-public)/school/infrastructure/page.tsx
+++ src/app/[locale]/(main-public)/school/infrastructure/page.tsx
@@ -5,132 +5,10 @@ import ArticlePage from "../../_components/ArticlePage";
 import type { FC } from "react";
 import NextImage from "next/image";
 import type { NextPageProps } from "@/src/types";
+import { infrastructureCards } from "./data";
 import { setStaticParamsLocale } from "next-international/server";
 
-const infrastructureCards = [
-  {
-    big_screen: false,
-
-    title: "Smart Classes rooms",
-    description: "Modern & spacious classrooms with Interactive TV",
-    image: {
-      url: "https://images.pexels.com/photos/5905445/pexels-photo-5905445.jpeg?auto=compress&cs=tinysrgb&w=600",
-      alt: "smart class",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Sports facilities",
-    description:
-      "Outdoor Facility for Football, Basketball and Cricket. Indoor Facility for Chess, Gymnastics, Badminton, Yoga & Table Tennis.",
-    image: {
-      url: "https://images.pexels.com/photos/18644293/pexels-photo-18644293/free-photo-of-aerial-drone-view-of-a-rugby-stadium-from-top.jpeg?auto=compress&cs=tinysrgb&w=600",
-      alt: "Sports facilities",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Dance Room",
-    description:
-      "Indian Classical, Folk Dance, Contemporary and Modern Creative Dance etc.",
-    image: {
-      url: "https://images.pexels.com/photos/358010/pexels-photo-358010.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Dance Room",
-    },
-  },
-  {
-    big_screen: false,
-    title: "Art Room",
-    description:
-      "Spacious Air-conditioned well equipped Art Studio to enlarge the creative minds.",
-    image: {
-      url: "https://images.pexels.com/photos/383568/pexels-photo-383568.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Art Room",
-    },
-  },
-  {
-    big_screen: false,
-    title: "Music Room",
-    description:
-      "Facilities for learning Piano, Guitar, Tabla, Violin, Vocal - Indian & Western",
-    image: {
-      url: "https://images.pexels.com/photos/164938/pexels-photo-164938.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Music Room",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Computer Lab",
-    description:
-      "7 fully Air-conditioned Computer Labs with 1:1 computer for every student. tie-up with NIIT",
-    image: {
-      url: "https://images.pexels.com/photos/3747486/pexels-photo-3747486.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Computer Lab",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Library",
-    description:
-      "5 airconditioned library to cater 200 students at a time. It is well equipped with 50,000 books, numerous magazines, journals and newspapers with more than 1000 DVDs / CDs to browse through. It has excellent collection of latest Reference books for Competitive Entrance Examinations.",
-    image: {
-      url: "https://images.pexels.com/photos/1370298/pexels-photo-1370298.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Library",
-    },
-  },
-  {
-    big_screen: false,
-    title: "CCTV surveillance system",
-    description:
-      "School is under CCTV surveillance & 24×7 Security Guards to take care of the safety of students. School also has the latest system for fire safety and detection.",
-    image: {
-      url: "https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "CCTV surveillance system",
-    },
-  },
-  {
-    big_screen: false,
-    title: "Science Lab",
-    description:
-      "Twenty-First Century Science Labs to emphasise the modern infrastructure of the school",
-    image: {
-      url: "https://images.pexels.com/photos/4033148/pexels-photo-4033148.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Science Lab",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Infirmary",
-    description:
-      "An airconditioned efficient Infirmary with trained nurse to cater daily medical emergencies. Provision for online medical consultation with trained Doctors of the city.",
-    image: {
-      url: "https://images.pexels.com/photos/13427358/pexels-photo-13427358.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Infirmary",
-    },
-  },
-  {
-    big_screen: true,
-    title: "Internet and WIFI facility",
-    description:
-      "Wi-Fi Campus for conducting Online Exams to prepare students for Competitive Exams.",
-    image: {
-      url: "https://images.pexels.com/photos/13092788/pexels-photo-13092788.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Internet and WIFI facility",
-    },
-  },
-  {
-    big_screen: false,
-    title: "Fire-Fighting Devices",
-    description:
-      "Fire Control Panel, Smoke Detectors, Hooters, Manual Call Point, Water Pump, Reservoir.",
-    image: {
-      url: "https://images.pexels.com/photos/189474/pexels-photo-189474.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      alt: "Fire-Fighting Devices",
-    },
-  },
-] as const;
-
-const Infrastructure: FC<NextPageProps> = async ({ params: { locale } }) => {
+const Infrastructure: FC<NextPageProps> = ({ params: { locale } }) => {
   setStaticParamsLocale(locale);
 
   return (
@@ -141,10 +19,13 @@ const Infrastructure: FC<NextPageProps> = async ({ params: { locale } }) => {
       <div className="flex justify-between">
         <ArticleHeading id="infrastructure">Infrastructure</ArticleHeading>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 ">
-        {infrastructureCards.map((card, big_screen) => (
-          <div className="rounded p-2 transition-all duration-700 hover:scale-110">
-            <Card key={card.image.alt}>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2  ">
+        {infrastructureCards.map(card => (
+          <div
+            key={card.image.alt}
+            className="rounded p-2 transition-all duration-700 hover:scale-110"
+          >
+            <Card>
               {card.big_screen === false ? (
                 <>
                   <CardHeader className=" flex justify-center ">
