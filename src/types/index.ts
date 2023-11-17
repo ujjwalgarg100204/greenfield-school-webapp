3c06b6ff0f2ffd1018eb5ac07d6b76f0d83fcd41
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2023-11-17 10:59:58 +0530

    Add Notification type and LayoutProps type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,11 @@
 import type { ReactNode } from "react";
 
 export type NextPageProps = { params: { locale: string } };
+export type Notification = {
+  title: string;
+  description: string;
+  isUrgent: boolean;
+};
 declare module "react" {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     // extends React's HTMLAttributes
@@ -8,3 +13,5 @@ declare module "react" {
     webkitdirectory?: string;
   }
 }
+
+export type LayoutProps = { children: ReactNode };
