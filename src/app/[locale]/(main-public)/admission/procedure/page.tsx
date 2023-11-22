41c4aca9edd0d0cbae47a18737c03a053a98fd1b
Priyansh-Kotak <priyanshkotak1@gmail.com>
2023-11-22 19:37:32 +0530

    All the lorems are converted to real text messages and the changes told by Jagdeesh are inplimented

--- src/app/[locale]/(main-public)/admission/procedure/page.tsx
+++ src/app/[locale]/(main-public)/admission/procedure/page.tsx
@@ -1,12 +1,11 @@
 import ArticleHeading from "@/src/components/ArticleHeading";
 import ArticleList from "@/src/components/ArticleList";
+import ArticlePage from "../../_components/ArticlePage";
+import type { FC } from "react";
 import type { NextPageProps } from "@/src/types";
 import { setStaticParamsLocale } from "next-international/server";
-import type { FC } from "react";
-import ArticlePage from "../../_components/ArticlePage";
 
 const procedure = [
-  "The list of selected candidates will be put up on the Notice Board at the Admission Office and on the School website www.greenfield-international.com.",
   "The Admission procedure will be intimated to the parents of the selected candidates. It is mandatory for both the parents and local / legal guardian to be present to complete various admission formalities.",
   "For students seeking admission to Class I onwards, it is mandatory to submit the Original Transfer Certificate from the last school attended.",
   "Fees once paid at the time of admission is non refundable, except the Caution Money (Refundable) in case of withdrawal.",
