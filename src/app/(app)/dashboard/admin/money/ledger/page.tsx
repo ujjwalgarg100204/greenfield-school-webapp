f5330cea30a7401715ba98269bf369c44c1e428a
Priyansh-Kotak <priyanshkotak1@gmail.com>
2024-02-29 21:53:08 +0530

    admin dashboard added

---
+++ src/app/(app)/dashboard/admin/money/ledger/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <div>{/* <LinkNavbar /> */}</div>
+            <div className="  m-5 w-full  rounded-lg bg-green-100">
+                <h1>you are under ledger section</h1>
+            </div>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(app)/dashboard/admin/money/setup/page.tsx
@@ -0,0 +1,12 @@
+const Setup = () => {
+    return (
+        <div>
+            <div>{/* <LinkNavbar /> */}</div>
+            <div className="  m-5 w-full  rounded-lg bg-green-100">
+                <h1>you are under setup section</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Setup;
