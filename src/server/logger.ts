26e13960522f83412a6bf8643f687ca419cedaab
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-03-07 23:34:14 +0530

    add logger to project

---
+++ src/server/logger.ts
@@ -0,0 +1,23 @@
+import { env } from "process";
+import winston from "winston";
+
+export const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: { service: "user-service" },
+    transports: [
+        new winston.transports.File({
+            filename: "logs/error.log",
+            level: "error",
+        }),
+    ],
+});
+
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (env.NODE_ENV !== "production") {
+    logger.add(
+        new winston.transports.Console({ format: winston.format.simple() }),
+    );
+}
