f2eb55fc760346b52f5f7b9ce692acac870b4838
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-03-07 23:37:24 +0530

    modify useSelectedOption in drawer
    initially useSelectedOption worked by comparing current links to mapped link
    in drawer object, which would prevent any deeply nested page, without updating
    drawer object
    Fix: Now it tries to find the best suitable option based on best url match
    algorithm

--- src/app/(app)/dashboard/_components/Drawer/useSelectedOption.tsx
+++ src/app/(app)/dashboard/_components/Drawer/useSelectedOption.tsx
@@ -1,7 +1,22 @@
 "use client";
 
-import type { DrawerItem } from ".";
+import type { DrawerItem, SubDrawerItem } from ".";
 import { usePathname } from "next/navigation";
+
+const getSelectedSubItem = (
+    path: string,
+    { subItems }: { subItems: SubDrawerItem[] },
+): SubDrawerItem | null => {
+    let selectedSubItem: SubDrawerItem | null = null;
+    let maxLen = Number.MIN_VALUE;
+    for (const subItem of subItems) {
+        if (path.includes(subItem.href) && subItem.href.length > maxLen) {
+            selectedSubItem = subItem;
+            maxLen = Math.max(maxLen, subItem.href.length);
+        }
+    }
+    return selectedSubItem;
+};
 
 const useSelectedDrawerItem = (items: DrawerItem[]) => {
     const path = usePathname();
@@ -10,16 +25,15 @@ const useSelectedDrawerItem = (items: DrawerItem[]) => {
         if ("href" in item) {
             return item.href === path;
         } else if ("subItems" in item) {
-            return item.subItems.some(subItem => subItem.href === path);
+            return !!getSelectedSubItem(path, item);
         } else return false;
     });
-
     if (!selectedItem) throw new Error("Invalid URL");
+
     const selectedSubItem =
         "subItems" in selectedItem
-            ? selectedItem.subItems.find(subItem => subItem.href === path)
+            ? getSelectedSubItem(path, selectedItem)
             : null;
-
     if (selectedSubItem === undefined) throw new Error("Invalid URL");
 
     return { item: selectedItem, subItem: selectedSubItem };
