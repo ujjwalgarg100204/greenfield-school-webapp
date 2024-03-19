781bbf3750dd577a9d6d16588f93b5fe34ee2ee3
Ujjwal Garg <ujjwalgarg100204@gmail.com>
2024-03-19 21:42:29 +0530

    move seo from component file to page.tsx file

--- src/app/(app)/(marketing)/school/school-transport/page.tsx
+++ src/app/(app)/(marketing)/school/school-transport/page.tsx
@@ -1,4 +1,28 @@
 import type { FC } from "react";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "School Transport",
+    description:
+        "Our school provides best School transport facility. Though our school is in prime locality with well knitted public transport and accessibility, we provide school transportation for easy access and Parents comfort. The School supervises for smooth & hassle-free service. The parent/ guardian should coordinate with the school office regarding the pick-up and drop-off location of the student. All the School buses follow the latest guidelines of transportation rules and are equipped with CCTV cameras & connected electronically to the control room through a GPS tracking system installed in the buses. The Buses are also equipped with first aid boxes & fire extinguishers. A lady attendant accompanies the junior most students in the vehicle for their proper care.",
+    keywords: [
+        "School transport facility",
+        "Prime locality",
+        "Public transport",
+        "Accessibility",
+        "Parents comfort",
+        "Supervised transportation",
+        "Hassle-free service",
+        "Pick-up and drop-off",
+        "Transportation rules",
+        "CCTV cameras",
+        "GPS tracking system",
+        "First aid boxes",
+        "Fire extinguishers",
+        "Lady attendant",
+        "Student safet",
+    ],
+};
 
 const SchoolTransportPage: FC = () => {
     return (
